0f7488556cb37ce6a5ee2d30806558669a0e0681
VKoltsova <taratutavika@gmail.com>
2022-03-17 13:16:38 +0300

    main file

---
+++ main.py
@@ -0,0 +1,50 @@
+from crud_1.create_1 import create_user
+from crud_1.read_1 import user_info, all_users_info
+
+
+user_emails =[]
+user_storage = {}
+users_storage = {}
+
+
+while True:
+
+    action = input('Please, enter create or read_all, or read_user, or update, or delete: ')
+
+    if action == 'create':
+        print('action = ', action)
+
+        email = input('Email: ')
+        name = input('Name: ')
+        password = input('Password: ')
+        phone = input('Phone: ')
+        # if email != users_storage[email]:
+        create_user(email,
+                    name,
+                    password,
+                    phone,
+                    user_emails,
+                    users_storage)
+        print('user_emails = ', user_emails)
+        print('users_storage = ', users_storage)
+
+    elif action == 'read_all':
+        print('action = ', action)
+        all_users_info(users_storage)
+
+    elif action == 'read_user':
+
+        user_e = input('Enter user email:')
+        message = user_info(user_e, user_emails, users_storage)
+
+        print('action = ', action)
+        print('User = ', message)
+
+    elif action == 'update':
+        print('action = ', action)
+
+    elif action == 'delete':
+        print('action = ', action)
+
+    else:
+        print('Please, re-enter: ')
